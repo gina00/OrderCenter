6fac6ec8a1eaea8ecf3659a119f9de49a8f851d7
gina00 <584525974@qq.com>
2018-10-22 17:18:19 +0800

    updata readme

--- README.md
+++ README.md
@@ -1,6 +1,48 @@
-# ordercenter-project
+## 项目说明
+##### 使用人员：
+IBOP系统的开发主要是供中国移动客服人员使用。
+##### 功能模块
+基于原有IBOP系统的订单中心重新划分功能模块
+##### 现有需求 
+采用有别于旧版的全新视觉效果，使用新的前端技术（原有系统使用DIV+CSS、JQuery、cvs管理项目代码）。
+此订单中心旨在引进新技术和新视觉来引起客户变革，从而慢慢推进整套系统的整改需求。
 
-> A Vue.js project
+## 前端技术
+1. vue
+2. element
+3. Webpack
+4. Express
+
+## 开发工具
+VScode，Git
+
+## 界面测试
+IE9及以上、Chrome浏览器
+
+## 开发环境依赖
+node v0.10.28+
+
+## 部署步骤
+1.  npm install 安装项目依赖包
+2.  node server/app 启动express 后台登录验证
+3.   npm run dev 运行项目，端口8080
+4.  用户名：admin 密码：abcd1234 
+
+## 项目前端效果：
+
+首页：
+![image](https://github.com/gina00/vue-project/blob/master/static/img/170535.jpg)
+
+一线营业员订单管理
+![image](https://github.com/gina00/vue-project/blob/master/static/img/170608.jpg)
+
+后台营业员订单管理
+![image](https://github.com/gina00/vue-project/blob/master/static/img/2180631.jpg)
+
+支付订单管理
+![image](https://github.com/gina00/vue-project/blob/master/static/img/170649.jpg)
+
+
 
 ## Build Setup
 
--- src/views/layout/Layout.vue
+++ src/views/layout/Layout.vue
@@ -15,46 +15,40 @@
 </template>
 
 <script>
-import {
-    HeaderItem,
-    SideBar,
-    AppMain
-} from './components'
+import { HeaderItem, SideBar, AppMain } from "./components";
 //import SideBar from './components/Sidebar/index'
 //import AppMain from './components/AppMain'
 //import HeaderItem from './components/Header/index'
 export default {
-    components: {
-        'header-item': HeaderItem,
-        'side-bar': SideBar,
-        'app-main': AppMain
-
-    }
-
-}
+  components: {
+    "header-item": HeaderItem,
+    "side-bar": SideBar,
+    "app-main": AppMain
+  }
+};
 </script>
 
 <style lang="scss" scoped>
 .main-content {
-    min-height: 800px;
-    -webkit-transition: margin-left .28s;
-    transition: margin-left .28s;
-    margin-left: 230px;
-    position: relative;
-    top: 60px;
-    background: #f5f7fa;
-    padding: 20px 30px;
-    text-align: left;
+  min-height: 800px;
+  -webkit-transition: margin-left 0.28s;
+  transition: margin-left 0.28s;
+  margin-left: 230px;
+  position: relative;
+  top: 60px;
+  background: #f5f7fa;
+  padding: 20px 30px;
+  text-align: left;
 }
 
 .container {
-    header {
-        height: 60px;
-        background: #0486fe;
-        position: fixed;
-        top: 0;
-        width: 100%;
-        z-index: 1000;
-    }
+  header {
+    height: 60px;
+    background: #0486fe;
+    position: fixed;
+    top: 0;
+    width: 100%;
+    z-index: 1000;
+  }
 }
 </style>
